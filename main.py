139d92c68c843c5e628bbd615da2001c8e7d11bb
CoyoteHawk <44215324+CoyoteHawk@users.noreply.github.com>
2022-08-03 13:06:58 -0400

    added gitignore

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import dearpygui as dpg
+import sys
+import time
+import json
+
+
+
+"""
+Module Docstring
+"""
+
+__author__ = "Sela"
+__version__ = "1.0.0"
+__license__ = "GPLv3"
+
+
+def main():
+    print('Placeholder')
+
+
+
+
+if __name__ == "__main__":
+    
+    main()
